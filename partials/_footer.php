1b140fbda2d605e5501ca89fe572def202028561
Filip Bogdanovic <61761195+fickezapad@users.noreply.github.com>
2021-11-24 19:05:50 +0100

    initial commit

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2021 Designed by <a href="https://github.com/fickezapad" target="_blank" rel="noopener noreferrer">@fickezapad</a></p>
+</div>
